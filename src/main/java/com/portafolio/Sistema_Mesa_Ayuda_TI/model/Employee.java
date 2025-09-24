e38dd11b52661acf6208048a6f3230eff4a228a7
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-09-24 11:45:21 -0500

    docs: añadir documentación Javadoc a Employee

--- src/main/java/com/portafolio/Sistema_Mesa_Ayuda_TI/model/Employee.java
+++ src/main/java/com/portafolio/Sistema_Mesa_Ayuda_TI/model/Employee.java
@@ -5,14 +5,25 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Entity that represents a company employee.
+ * An employee can create multiple support tickets.
+ */
 @Entity
 @Table(name = "employees")
 @Getter
 @Setter
 public class Employee extends Person {
 
+    /**
+     * Employee's email address.
+     */
     private String email;
 
+    /**
+     * Department where the employee works.
+     */
     private String deparment;
 }
+
 
