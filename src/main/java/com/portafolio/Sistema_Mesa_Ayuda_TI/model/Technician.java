7f2cccb9c8f4c20a3f14bcf9e73a9407695c11d4
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-09-24 11:45:22 -0500

    docs: añadir documentación Javadoc a Technician

--- src/main/java/com/portafolio/Sistema_Mesa_Ayuda_TI/model/Technician.java
+++ src/main/java/com/portafolio/Sistema_Mesa_Ayuda_TI/model/Technician.java
@@ -5,12 +5,21 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Entity that represents an IT technician.
+ * A technician may have different specialties
+ * and can manage multiple support tickets.
+ */
 @Entity
 @Table(name = "technicians")
 @Getter
 @Setter
 public class Technician extends Person {
 
+    /**
+     * Technician's specialties (e.g., Hardware, Software, Networking).
+     */
     private String specialties;
 }
+
 
