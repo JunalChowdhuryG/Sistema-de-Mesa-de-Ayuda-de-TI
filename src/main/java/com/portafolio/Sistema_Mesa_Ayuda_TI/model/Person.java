efd0e3d5efbac65246af90ebde9ad3897e187834
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-09-24 11:45:20 -0500

    docs: añadir documentación Javadoc a Person

--- src/main/java/com/portafolio/Sistema_Mesa_Ayuda_TI/model/Person.java
+++ src/main/java/com/portafolio/Sistema_Mesa_Ayuda_TI/model/Person.java
@@ -5,14 +5,24 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Abstract class that represents a person in the system.
+ * Serves as a base class for employees and technicians.
+ */
 @MappedSuperclass
 @Getter
 @Setter
 public class Person extends BaseEntity {
 
+    /**
+     * First name of the person.
+     */
     @Column(name = "first_name")
     private String firstName;
 
+    /**
+     * Last name of the person.
+     */
     @Column(name = "last_name")
     private String lastName;
 }
