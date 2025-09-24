d1a6fad6397678a70a2a0f4b139a98dd71ddf600
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-09-24 11:22:37 -0500

    feat: crear entidad base BaseEntity con id autogenerado

---
+++ src/main/java/com/portafolio/Sistema_Mesa_Ayuda_TI/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.portafolio.Sistema_Mesa_Ayuda_TI.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import java.io.Serializable;
+
+@Setter
+@Getter
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
+
